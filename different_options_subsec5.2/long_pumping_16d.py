2b3066fca17b06b57c26da425667a7500edbb3e4
Sona Salehian <sona.salehianghamsari@uni.lu>
2023-11-02 17:24:23 +0100

    20d to 16d

--- different_options_subsec5.2/long_pumping_16d.py
+++ different_options_subsec5.2/long_pumping_16d.py
@@ -3,7 +3,7 @@ sys.path.insert(0, "../anderson_junction_subsec5.1")
 from default_parameters import parameters
 from model import solve
 
-parameters["T"] = 1728000  # 20 days
-parameters["output_dir"] = '../output/long_pumping_20d'
+parameters["T"] = 1382400  # 16 days
+parameters["output_dir"] = '../output/long_pumping_16d'
 
 solve(parameters)
